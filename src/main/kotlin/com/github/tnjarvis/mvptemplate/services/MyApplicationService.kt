062afbb96945ea451fda026198eef3bef16e7ecb
GitHub Action <action@github.com>
2022-12-13 03:39:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/tnjarvis/mvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tnjarvis/mvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tnjarvis.mvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tnjarvis.mvptemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tnjarvis/mvptemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tnjarvis/mvptemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tnjarvis.mvptemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tnjarvis.mvptemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
